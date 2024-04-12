4c0ba5cd4e635389df9cdc526ee362aef66585e7
Ignat Balakirev <ivbignal@gmail.com>
2024-04-12 16:14:39 +0300

    fix: release builds for windows and ubuntu

--- pack.sh
+++ pack.sh
@@ -10,6 +10,15 @@ while getopts 'p:' flag; do
 done
 
 mkdir -p art
+
 if [ $platform = "macos-latest" ]; then
   tar -czf art/LiveBeatMonitor.tar.gz dist/LiveBeatMonitor.app
+fi
+
+if [ $platform = "windows-latest" ]; then
+  zip -r art/LiveBeatMonitor.zip dist
+fi
+
+if [ $platform = "ubuntu-latest" ]; then
+  tar -czf art/LiveBeatMonitor.tar.gz dist
 fi
